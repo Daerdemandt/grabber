7413154d87c9d0a22d13e90e02c2e133ea14c14a
Roman Kuzmichev <daerdemandt@gmail.com>
2015-06-16 15:50:36 +0300

    Made use of some OOP for 1xbet

--- 1xbet.py
+++ 1xbet.py
@@ -6,62 +6,61 @@
 # Several assumptions about page format are made. If any of them is violated, ParseError is raised.
 
 from datetime import datetime
+import bs4
+from misc import BetParser, UTC
 
-from misc import *
+class Parser(BetParser):
+	def set_url(self):
+		self.url = 'https://1-x-bet.com/line/KiberSport/'
+	def set_resource_name(self):
+		self.resource_name = '1xbet'
+	def set_resource_type(self):
+		self.resource_type = 'html'
 
-set_url('https://1-x-bet.com/line/KiberSport/')
-
-URL3.set('https://1-x-bet.com/line/KiberSport/')
-print(URL3.url)
-
-def get_data():
-	yield from extract_data(get_parsed_html())
-
-def extract_data(whole_soup):
-#	whole_soup = read_soup(filename)
-	body = whole_soup.html.body
-	bets_table = list(soup_find_only(body, class_="hot_live_bet"))
-	# Last etnry is just a string, is it?
-	parse_assert(isinstance(bets_table[-1], element.NavigableString))
-	# Last entry is just a string so we don't parse it
-	for bet in bets_table[:-1]:
-		bet_data = {}
+	def get_data(self):
+		body = self.resource.html.body
+		bets_table = list(self.find_only(body, class_="hot_live_bet"))
+		# Last etnry is just a string, is it?
+		self.assume(isinstance(bets_table[-1], bs4.element.NavigableString))
+		# Last entry is just a string so we don't parse it
+		for bet in bets_table[:-1]:
+			bet_data = {}
+			
+			# Let's start with competing teams' names
+			game_name = self.find_only(bet, class_='gname hotGameTitle')
+			game_name = game_name.string.strip()
+			# We assume that teams' names don't contain '—'
+			self.assume(1 == game_name.count('—'))
+			bet_data['team1'], bet_data['team2'] = game_name.split(' — ')
 		
-		# Let's start with competing teams' names
-		game_name = soup_find_only(bet, class_='gname hotGameTitle')
-		game_name = game_name.string.strip()
-		# We assume that teams' names don't contain '—'
-		parse_assert(1 == game_name.count('—'))
-		bet_data['team1'], bet_data['team2'] = game_name.split(' — ')
-		
-		# Now, lets go for time and date. We use UTC, and so do they, apparently
-		# I don't know why they've called it this way.
-		datetime_string = str(soup_find_only(bet, 'td', class_='score'))
+			# Now, lets go for time and date. We use UTC, and so do they, apparently
+			# I don't know why they've called it this way.
+			datetime_string = str(self.find_only(bet, 'td', class_='score'))
 			# We'll have to do it the ugly way. Use '>' and '<' as separators.
-		try:
-			datetime_string = datetime_string.replace('>', '<').split('<')[2]
-			date, time = datetime_string.split(' | ')
-			day, month = date.split('.')
-			day, month = int(day), int(month)
-			year = datetime.now().year
-			# Bet info is provided a bit in advance.
-			# We shall work properly on a new year.
-			if 12 == datetime.now().month and 1 == month:
-				year += 1
-			hour, minute = time.split(':')
-			hour, minute = int(hour), int(minute)
-			bet_data['datetime'] = datetime(year, month, day, hour, minute, tzinfo=UTC())
-		except ValueError:
-			parse_assert()	
-		# Finally, let's get odds we need
-		def get_odds(betname):
-			return float(soup_find_only(bet, 'div', {'data-betname':betname}).string.strip())
-		bet_data['team1_wins'] = get_odds('П1')
-		bet_data['team2_wins'] = get_odds('П2')
-		yield bet_data
+			try:
+				datetime_string = datetime_string.replace('>', '<').split('<')[2]
+				date, time = datetime_string.split(' | ')
+				day, month = date.split('.')
+				day, month = int(day), int(month)
+				year = datetime.now().year
+				# Bet info is provided a bit in advance.
+				# We shall work properly on a new year.
+				if 12 == datetime.now().month and 1 == month:
+					year += 1
+				hour, minute = time.split(':')
+				hour, minute = int(hour), int(minute)
+				bet_data['datetime'] = datetime(year, month, day, hour, minute, tzinfo=UTC())
+			except ValueError:
+				self.assume()	
+			# Finally, let's get odds we need
+			def get_odds(betname):
+				return float(self.find_only(bet, 'div', {'data-betname':betname}).string.strip())
+			bet_data['team1_wins'] = get_odds('П1')
+			bet_data['team2_wins'] = get_odds('П2')
+			yield bet_data
 		
 def main():
-	print_bets(get_data())
+	Parser().print_results_pretty()
 			
 if __name__ == "__main__":
 	main()
--- misc.py
+++ misc.py
@@ -6,6 +6,8 @@ from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup, element
 import json
 from hashlib import md5
+import csv
+import sys
 
 class UTC(tzinfo):
 	"""UTC"""
@@ -23,6 +25,106 @@ class ParseError(Exception):
 def read_soup(filename):
 	with open(filename) as input_file:
 		return BeautifulSoup(input_file)
+
+from abc import ABCMeta, abstractmethod
+
+class GenericParser(object):
+	__metaclass__ = ABCMeta
+
+	@abstractmethod
+	def get_data(self):
+		pass
+	@abstractmethod
+	def set_url(self):
+		pass
+	@abstractmethod
+	def set_resource_name(self):
+		pass
+	@abstractmethod
+	def set_resource_type(self):
+		pass
+	@abstractmethod
+	def set_fields_we_need(self):
+		pass
+	
+	def __init__(self):
+		self.set_url()
+
+		self.set_resource_name()
+		self.set_resource_type()
+		
+		resource_types = ['html', 'json', None]
+		if hasattr(self, 'valid_resource_types'):
+			self.valid_resource_types |= set(resource_types)
+		else:
+			self.valid_resource_types = set(resource_types)
+		assert self.resource_type in self.valid_resource_types
+		
+		resource_raw = fetch(self.url)
+		self.resource = None
+		# Time to do some generic parsing
+		if self.resource_type == 'html':
+			self.resource = BeautifulSoup(resource_raw)
+		elif self.resource_type == 'json':
+			self.resource = json.loads(resource_raw)
+		
+		self.set_fields_we_need()
+		
+		self.results = self.get_data()
+	
+	def find_only(self, where, *args, **kwargs): # beatifulsoup-only
+		assert self.resource_type == 'html'
+		results = where.find_all(*args, **kwargs)
+		self.assume(len(results) == 1)
+		return results[0]
+	
+	def assume(self, check=False):
+		if not check:
+			raise ParseError('Error: page at ' + URL + " doesn't comply to assumed format")
+
+	def do_static_cast(self):
+		self.result = list(self.result)
+
+	@abstractmethod
+	def print_result_pretty(self, result, output_file):
+		pass
+	
+	# TODO: check if this rerouting magic works.
+	def print_results_pretty(self, output_file=sys.stdout):
+		stdout_save = sys.stdout
+		sys.stdout = output_file
+		if self.resource_name:
+			print('From', self.resource_name, ':')
+		for result in self.results:
+			self.print_result_pretty(result)
+		sys.stdout = stdout_save
+
+	def print_results_csv(self, csvfile=sys.stdout):
+		stdout_save = sys.stdout
+		sys.stdout = csvfile
+		writer = csv.writer(csvfile, dialect='unix')
+		for res in self.results:
+			assert issubclass(res.__class__, (list, tuple, dict))
+			if issubclass(res.__class__, dict):
+				writer.writerow(tuple(res[field] for field in self.fields_we_need))
+			else:
+				writer.writerow(res)
+		sys.stdout = stdout_save
+
+class BetParser(GenericParser):	
+	def set_fields_we_need(self):
+		self.fields_we_need = ['datetime', 'team1', 'team2', 'team1_wins', 'team2_wins']
+	
+	def print_result_pretty(self, bet_data):
+		dt_string = bet_data['datetime'].strftime("%B %d, %Y %H:%M")
+		name_length = 20
+		t1_string = bet_data['team1'].rjust(name_length)
+		t2_string = bet_data['team2'].ljust(name_length)
+		odds_length = 5
+		p1_string = str(bet_data['team1_wins']).ljust(odds_length)
+		p2_string = str(bet_data['team2_wins']).ljust(odds_length)
+		print(dt_string, t1_string, 'vs', t2_string, p1_string, p2_string)
+
 
 tmpdir = '/tmp/bets_grabber'
 
@@ -112,6 +214,8 @@ def print_bets_simple(bets):
 def fetch(url):
 	source = urlopen(Request(url, headers={'User-Agent': 'Mozilla'}))
 	return source.readall().decode('utf-8')
+
+## further can be removed
 
 def get_parsed_html(url=None):
 	if not url:
